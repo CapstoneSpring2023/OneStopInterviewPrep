79630fd68d43224409c929455590254d0bf0fa35
nadxhielijuarez <nadxhielijuarez@gmail.com>
2023-02-20 21:28:33 -0600

    refactored
    File were moved around & grouped to make sense, dependencies fixed. Just UI, no backend ...

---
+++ src/pages/company_pages/microsoftPrep.js
@@ -0,0 +1,81 @@
+import React, {useState} from 'react'
+import ReviewList from '../../components/review_displays/ReviewList'
+import {Link } from "react-router-dom";
+import styled from "styled-components";
+//import VideoComp from '../components/VideoPlayer/VideoComp'
+import FeedbackView from '../../components/feedback_display/FeedbackView';
+import LeetCodeURL from '../../components/urls/LeetCodeURL'
+import HackerRankURL from '../../components/urls/HackerRankURL'
+import OtherURL from '../../components/urls/OtherURL'
+
+const LinkButton = styled.button `
+  cursor: pointer;
+  background-color: rgb(100, 0, 0);
+  width: 300px;
+  height: 150px;
+  padding: 5px 15px;
+  border-radius: 20px;
+  text-decoration: none;
+  color: white;
+  font-size: 40px;
+  font-family: "Lucida Console", "Courier New", monospace;
+
+  overflow-x: hidden;
+  float: left;
+`
+
+var companyName = <text>Microsoft</text>;
+
+const MicrosoftPrep = () => {
+    const [reviews, setReviews] = useState([])
+
+    localStorage.setItem("this-company", "Microsoft");
+
+  return (
+    <div class='company-page'>
+        <div class='company-header'>
+          <h1>Interview Preparation Resources For {companyName}</h1>
+        </div>
+        <div class='split'>
+          <div class='company-left'>
+            <div class='company-section'>
+              <h1>Our Coding Problems</h1>
+              <body>
+                Go to our <a href="/coding">practice coding problems</a> and select the "Sort By {companyName}" option.
+              </body>
+            </div>
+            <div class='company-section'>
+              <h1>HackerRank Links</h1>
+              <ul>
+                <HackerRankURL/>
+              </ul>
+            </div>
+          </div>
+          <div class='company-right'>
+            <div class='company-section'>
+              <h1>LeetCode Problems</h1>
+              <ul>
+                <LeetCodeURL/>
+              </ul>
+            </div>
+            <div class='company-section'>
+              <h1>More Resources</h1>
+              <ul>
+                <OtherURL/>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class='company-section'>
+          <h1>Youtube Playlist</h1>
+          {/* <VideoComp playListId = "PLi9RQVmJD2fYMiu6JOL3PiEVh_Is6jAin"/> */}
+        </div>
+        <div class="company-section reviewList">
+          <h1>Interview Reviews For {companyName}</h1>
+          <FeedbackView/>
+        </div>
+    </div>
+  )
+}
+
+export default MicrosoftPrep
