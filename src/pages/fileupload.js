97c6b82f781c5fc9b5da02ee23a91c867346e7a5
claytonkristiansen <clayton.kristiansen@hotmail.com>
2023-04-07 23:04:48 -0500

    Added file upload

--- src/pages/fileupload.js
+++ src/pages/fileupload.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
 const FileUpload = () => {
+    const [selectedFile, setSelectedFile] = React.useState(null);
+    const HandleSubmit = async(event) => {
+        event.preventDefault()
+        const formData = new FormData();
+        formData.append("videodata", selectedFile);
+        try {
+            const response = await axios({
+                method: "post",
+                url: "http://localhost:5000/upload",
+                data: formData,
+                headers: {"Content-Type": "multipart/form-data"}
+            })
+            console.log(response);
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
+    const handleFileSelect = (event) => {
+        setSelectedFile(event.target.files[0]);
+    }
+
     return (
         <div>
-            <p>Hello World!</p>
+            <form onSubmit={HandleSubmit}>
+                <input type='file' onChange={handleFileSelect}/>
+                <input type='submit' value='Upload File'/>
+            </form>
         </div>
     )
 }
