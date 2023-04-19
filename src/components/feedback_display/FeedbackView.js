4e76212c706b3b8e9613215ee43a106874ed3077
nadxhielijuarez <nadxhielijuarez@gmail.com>
2023-04-19 10:51:10 -0500

    reviews are partially implemented

--- src/components/feedback_display/FeedbackView.js
+++ src/components/feedback_display/FeedbackView.js
@@ -1,97 +1,98 @@
 import React, { useEffect, useState } from "react";
-// import loadingGif from "../../images/loading.gif";
+import loadingGif from "../../images/loading.gif";
 import upimg1 from "../../images/up1.png";
 import upimg2 from "../../images/up2.png";
 import downimg1 from "../../images/down1.png";
 import downimg2 from "../../images/down2.png";
+import { API } from 'aws-amplify';
+import { listReviews} from '../../graphql/queries';
 
 
-function FeedbackView({company}){
-    // const [reviewInfo, setReviewInfo] = useState(null);
-    // useEffect(() => {
-    //     fetch(dbAddress + "/getReviews",{
-    //     method: "GET"
-    //   }).then(response => {
-    //    if (response.type === 'opaque' || response.ok) {
-    //        response.json().then(revItems => {
-    //        setReviewInfo(revItems)
-    //      });
-    //    } 
-    //  }).catch(error => {
-    //    console.log("Error is: ", error)
-    //  });
-    // },[]);
-
-    // function upVote(id) {
-    //    // console.log("upvote id: ", id)
-    //         fetch(dbAddress + '/upVote/' + id,{
-    //         method: "GET"
-    //     }).then(response => {
-    //         if (response.type === 'opaque' || response.ok) {
-    //             window.location.href = window.location.href;
-    //         } 
-    //     }).catch(error => {
-    //     console.log("Error is: ", error)
-    //     });
+function FeedbackView({companyIDs}){
+    const [reviewInfo, setReviewInfo] = useState(null);
+    useEffect(() => {
+        //
+        API.graphql({
+            query: listReviews
+          }).then(response => {
+            let reviewObj = response.data.listReviews.items
+            console.log("response is: ", reviewObj);
+            setReviewInfo(reviewObj);
+        }).catch(error => {
+          console.log("Error is: ", error)
+        });
         
-    // }
+    },[]);
 
-    // function downVote(id) {
-    //     // do something with the id of the review
-    //     //console.log("*downvote id: ", id)
-    //     fetch(dbAddress + '/downVote/' + id,{
-    //     method: "GET" 
-    //     }).then(response => {
-    //         if (response.type === 'opaque' || response.ok) {
-    //             window.location.href = window.location.href;
-    //         } 
-    //     }).catch(error => {
-    //         console.log("Error is: ", error)
-    //     });
+    function upVote(id) {
+       // console.log("upvote id: ", id)
+        //     fetch(dbAddress + '/upVote/' + id,{
+        //     method: "GET"
+        // }).then(response => {
+        //     if (response.type === 'opaque' || response.ok) {
+        //         window.location.href = window.location.href;
+        //     } 
+        // }).catch(error => {
+        // console.log("Error is: ", error)
+        // });
+        
+    }
+
+    function downVote(id) {
+        // do something with the id of the review
+        //console.log("*downvote id: ", id)
+        // fetch(dbAddress + '/downVote/' + id,{
+        // method: "GET" 
+        // }).then(response => {
+        //     if (response.type === 'opaque' || response.ok) {
+        //         window.location.href = window.location.href;
+        //     } 
+        // }).catch(error => {
+        //     console.log("Error is: ", error)
+        // });
        
-    // }
+    }
 
 
-    // if(reviewInfo == null ){
-    //     return(
-    //         <div>
-    //             <img src={loadingGif} alt="wait until the page loads"/>
-    //         </div>
-    //     )
-    // } else {
-    //     var reviewList = new Array();
-    //     var upImage = <img src={upimg2} alt="upvote" align="left"/>
-    //     // if statement to change which up image to use based on whether the user has voted
-    //     var downImage = <img src={downimg2} alt="upvote" align="right"/>
-    //     // if statement to change which down image to use
-    //     var thisCompany = localStorage.getItem("this-company");
-    //     /*the down vote and up votes dont display properly on some company pages */
-    //     reviewInfo.map( singleReviewItem => {
-    //         reviewList.push(
-    //             (!thisCompany.localeCompare(singleReviewItem.title)) ? 
-    //             (<div className="reviewBox">
-    //                 <h3>Name: {singleReviewItem.name} / {singleReviewItem.title}</h3>
-    //                 <h3>Tags: </h3>
-    //                 <body>
-    //                     {singleReviewItem.description}
-    //                 </body>
-    //                 <div class="votes up">
-    //                     <a onClick={() => { upVote(singleReviewItem.id)}}>{upImage}</a>{singleReviewItem.upVotes}
-    //                 </div>
-    //                 <div class="votes down">
-    //                     <a onClick={() => {downVote(singleReviewItem.id)}}>{downImage}</a>{singleReviewItem.downVotes}
-    //                 </div>
-    //             </div>)
-    //             : null
-    //         )
-    //     });
+    if(reviewInfo == null ){
+        return(
+            <div>
+                <img src={loadingGif} alt="wait until the page loads"/>
+            </div>
+        )
+    } else {
+        var reviewList = new Array();
+        var upImage = <img src={upimg2} alt="upvote" align="left"/>
+        // if statement to change which up image to use based on whether the user has voted
+        var downImage = <img src={downimg2} alt="upvote" align="right"/>
+        // if statement to change which down image to use
+        var thisCompany = localStorage.getItem("this-company");
+        /*the down vote and up votes dont display properly on some company pages */
+        reviewInfo.map( singleReviewItem => {
+            reviewList.push(
+                (!thisCompany.localeCompare(singleReviewItem.companyID)) ? 
+                (<div className="reviewBox">
+                    <h3>Name: Anonymous </h3>
+                    <body>
+                        {singleReviewItem.review}
+                    </body>
+                    <div class="votes up">
+                        <a onClick={() => { upVote(singleReviewItem.id)}}>{upImage}</a>{singleReviewItem.upVotes}
+                    </div>
+                    <div class="votes down">
+                        <a onClick={() => {downVote(singleReviewItem.id)}}>{downImage}</a>{singleReviewItem.downVotes}
+                    </div>
+                </div>)
+                : null
+            )
+        });
         
-    //     return(
-    //         <div>
-    //             {reviewList}
-    //         </div>
-    //     )
-    // }
+        return(
+            <div>
+                {reviewList}
+            </div>
+        )
+    }
 
 }
 
