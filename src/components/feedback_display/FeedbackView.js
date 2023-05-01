ea491017ead7ec574b84e720350dca0da02b528e
nadxhielijuarez <nadxhielijuarez@gmail.com>
2023-05-01 12:58:05 -0500

    semi-working like button
    the value displayed should automatically update, and the state of the like button (once liked) should not reset to unliked when refreshing

--- src/components/feedback_display/FeedbackView.js
+++ src/components/feedback_display/FeedbackView.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import loadingGif from "../../images/loading.gif";
-import upimg1 from "../../images/up1.png";
-import upimg2 from "../../images/up2.png";
+import disliked from "../../images/disliked.png";
+import liked from "../../images/liked.png"
+import like_feedback from "../../images/like_feedback.gif"
 import { API } from 'aws-amplify';
 import { listReviews} from '../../graphql/queries';
 import { updateReview } from '../../graphql/mutations';
@@ -9,6 +10,13 @@ import { updateReview } from '../../graphql/mutations';
 
 function FeedbackView({companyIDs}){
     const [reviewInfo, setReviewInfo] = useState(null);
+    const [likeimg, setLikeImg] = useState(disliked);
+    const [rating, setRating] = useState(null);
+    const like_img_state = React.createRef(false);
+    var like_disp;
+    localStorage.setItem('like_status', false);
+
+
     useEffect(() => {
         //
         API.graphql({
@@ -43,14 +51,17 @@ function FeedbackView({companyIDs}){
         }
         return level_str;
     }
+
+
+
+    
     function upVote(id) {
+        //console.log("State is set to: ", setlike.state);
         var rev_obj = reviewInfo.filter(reviewObj => reviewObj.id === id);
         var rev_to_upvote = rev_obj[0]
         var new_rating = rev_to_upvote.rating + 1;
-        // console.log("review to upvote: ", rev_to_upvote)
-        // console.log(" new rating is: ", new_rating, " new rating type: ", typeof(new_rating))
-        // console.log("its id is: ", rev_to_upvote.id)
-        /* User Validation needed */
+        /* need to set states... no need to validate user*/
+        document.getElementsByClassName("votes").src = liked
         API.graphql({
             query: updateReview,
             variables:{
@@ -64,7 +75,31 @@ function FeedbackView({companyIDs}){
         }).catch(err => {
             console.log("Review update query in FeedbackView.js failed error is: ", err)
         })
-        window.location.reload();
+       // window.location.reload();
+    }
+
+    function downvote(id){
+        var rev_obj = reviewInfo.filter(reviewObj => reviewObj.id === id);
+        var rev_to_upvote = rev_obj[0]
+        var new_rating = rev_to_upvote.rating - 1;
+        //console.log("State is set to: ", setlike);
+        /* need to set states... no need to validate user*/
+        document.getElementsByClassName("votes").src = disliked;
+        API.graphql({
+            query: updateReview,
+            variables:{
+                input: {
+                    id: rev_to_upvote.id,
+                    rating: new_rating
+                }
+            }
+        }).then(res => {
+          //  console.log(res);
+        }).catch(err => {
+            console.log("Review update query in FeedbackView.js failed error is: ", err)
+        })
+        //window.location.reload();
+
     }
 
     function display_rating(id){
@@ -74,16 +109,54 @@ function FeedbackView({companyIDs}){
     }
 
 
+    function handleLike(id){
+        console.log("liked_img_state is: ", like_img_state.current);
+        if(like_img_state.current === true){
+            like_img_state.current = false;
+            localStorage.setItem('like_status', false);
+            console.log("disliked.")
+            downvote(id);  
+        } else{
+            like_img_state.current = true;
+            localStorage.setItem('like_status', true);
+            console.log("liked.")
+            upVote(id);
+        }
+        
+    }
 
-    if(reviewInfo == null ){
+    useEffect(() => {
+        var liked_status = localStorage.getItem('like_status');
+        console.log("IN USE EFFECT, liked status", liked_status);
+        if (liked_status === true){
+            setLikeImg(liked);
+        } else {
+            setLikeImg(disliked);
+        }
+
+    })
+
+    function like(e) {
+        var cur_img = e.target.getAttribute('src');
+        if(cur_img === disliked || like_img_state.current === false){
+            e.target.setAttribute( 'src', liked);
+            e.target.setAttribute('alt', 'liked');
+        } else if (cur_img === liked || like_img_state.current === true){
+            e.target.setAttribute( 'src', disliked);
+            e.target.setAttribute('alt', 'disliked');
+        }
+    }
+    like_disp = <img class = "votes" src={likeimg} alt="upvote" align="left"  ref={like_img_state} onClick={like}/>
+
+    if(reviewInfo === null ){
         return(
             <div>
                 <img src={loadingGif} alt="wait until the page loads"/>
             </div>
         )
     } else {
+        console.log("liked_img_state is: ", like_img_state.current);
         var reviewList = new Array();
-        var upImage = <img class = "votes" src={upimg2} alt="upvote" align="left"/>
         // if statement to change which up image to use based on whether the user has voted
         var thisCompany = localStorage.getItem("this-company");
         /*the down vote and up votes dont display properly on some company pages */
@@ -95,7 +168,7 @@ function FeedbackView({companyIDs}){
                     <h3>Level: {display_level(singleReviewItem.level)}</h3>
 
                     <div class="votes-up">
-                        <a onClick={() => { upVote(singleReviewItem.id)}}>{upImage}</a>{singleReviewItem.upVotes}
+                        <a onClick={() => {handleLike(singleReviewItem.id)}}>{like_disp}</a>{singleReviewItem.upVotes}
                         <h3>{display_rating(singleReviewItem.id)}</h3>
                     </div>
 
